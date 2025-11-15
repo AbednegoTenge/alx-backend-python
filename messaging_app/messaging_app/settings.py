354e3b82ef11d68afe7fd50b1b29b7a0ed246558
AbednegoTenge <abednegotenge180@gmail.com>
2025-11-15 15:25:32 +0000

    adding django project

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q39#tcg5vyqi5tou!1ui1_9ffc544wvx^7awxbb&38o+qmw!s8'
+SECRET_KEY = 'django-insecure-_s9g$ls92f+==f-oty-)i3_9+em3e-pe1%kvl(!of9v1edi*0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
