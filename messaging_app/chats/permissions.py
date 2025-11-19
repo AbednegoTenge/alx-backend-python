64b052b893bdd81de41f5ac0e23226d62ea8a7e6
AbednegoTenge <abednegotenge180@gmail.com>
2025-11-19 21:20:57 +0000

    adding permision file to set up custom permission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsMessageSenderOrParticipant(BasePermission):
+class IsMessageSenderOrParticipant(permissions.BasePermission):
 
 
     def has_object_permission(self, request, view, obj):
