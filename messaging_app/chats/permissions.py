ed81030ef6e16ff2e4fbf5218601a61040055bb9
AbednegoTenge <abednegotenge180@gmail.com>
2025-11-19 21:14:34 +0000

    adding permision file to set up custom permission

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsMessageSenderOrParticipant(BasePermission):
+
+
+    def has_object_permission(self, request, view, obj):
+        # Get the user from the request
+        user = request.user
+        # check if the user is the sender of the message or the recipient of the message
+        if user == obj.sender or user in obj.conversation.participants.all():
+            return True
+        return False
