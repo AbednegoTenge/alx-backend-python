c47ea716da62388ec1c3437402c3ddd18681218c
AbednegoTenge <abednegotenge180@gmail.com>
2025-11-24 18:20:10 +0000

    adding django-signals_orm-0x04

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
