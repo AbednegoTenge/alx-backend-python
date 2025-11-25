6198e5215f8f55b0e29372f8b3032e1ba4de373e
AbednegoTenge <abednegotenge180@gmail.com>
2025-11-25 17:08:36 +0000

    adding a custom manager to query for unread messages

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('delete_user/', views.delete_user, name='delete_user'),
     path('messages/<int:user_id>/', views.get_messages, name='get_messages'),
+    path('inbox/', views.unread_messages, name='unread_messages')
 ]
